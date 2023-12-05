f869a8e546a1442ba3888fc1a58ed7b09c61d49c
hyunmii <hyunmiiyu@gmail.com>
2023-12-05 11:32:25 +0900

    코네틱 모바일 gnb

--- 코네틱/konetic/js/konetic/user/ui-konetic_new.js
+++ 코네틱/konetic/js/konetic/user/ui-konetic_new.js
@@ -1,13 +1,4 @@
 $(function(){
-	//공통 레이아웃 스크립트	
-
-	//메인 헤더 배경 On/Off
-	// 202311 웹접근성
-	$(".wrap-header").on("mouseover focusin",function(){		
-		$(this).addClass('on');
-	}).on("mouseout focusout",function(){
-		$(this).removeClass('on');
-	})
 
 	// Gnb메뉴 드롭다운
 	$('.depth1 > li').on('mouseover keyup',function(){
@@ -21,9 +12,9 @@ $(function(){
 		$('.depth1 > li > a').removeClass('on');
 		$('.depth1 > li .sub_menu').removeClass('selected');
 	});
-	// 202311 웹접근성
 	$('.depth2 li:last').focusout(function(){
 		$('.depth1 > li > a').removeClass('on');
 		$('.depth1 > li .sub_menu').removeClass('selected');		
 	});
+
 });
