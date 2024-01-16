a3ed8a329e760496384f05b0927640bc3f8807f2
hyunmii <hyunmiiyu@gmail.com>
2024-01-16 11:05:50 +0900

    이민재단 회원가입 모바일 수정

--- 이민재단/kiiptest/js/layout.js
+++ 이민재단/kiiptest/js/layout.js
@@ -5,12 +5,14 @@ function layoutEvent() {
 		$(document).on('mouseover focusin', '.gnb>li>a', function () {//헤더 영역에 마우스오버하면
 			$(this).parent('li').addClass('on');
 			$('.gnb-bg').addClass('on');
+			$('#container').css('overflow-x','hidden');
 			$('#header .depth2').addClass('on');//2뎁스 열림
 			$(this).on('mouseleave', function () {
 				$(this).parent('li').removeClass('on');
 			});
 			$('#header .gnb-wrap').on('mouseleave', function () {//헤더 영역에서 마우스아웃하면
 				$('.gnb-bg').removeClass('on');
+				$('#container').css('overflow-x','visible');
 				$('#header .depth2').removeClass('on');//2뎁스 닫힘
 			});	
 		});
@@ -36,6 +38,7 @@ function layoutEvent() {
 
 	$('.depth2 li:last').focusout(function(){
 		$('.gnb-bg').removeClass('on');
+		$('#container').css('overflow-x','hidden');
 		$('#header .depth2').removeClass('on');		
 	});	
 
